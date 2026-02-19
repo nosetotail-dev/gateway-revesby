e4a4805cbac8a6cc0608e9ddafc89e31c126c455
Dyan <dyan@nosetotail.com.au>
2026-02-19 11:49:12 +0800

    change video to image

--- contact.php
+++ contact.php
@@ -35,7 +35,7 @@ get_header(); ?>
 				</div>
 			</div>
 			<div class="column">
-				<div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/1140656527?h=1b0269c967&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479&portrait=0&byline=0&title=0&rel=0" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share" referrerpolicy="strict-origin-when-cross-origin" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="61 Milperra Road, Revesby | Gateway Capital"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
+				<div class="image"><img src="<?php bloginfo ('stylesheet_directory'); ?>/shared/images/team.jpg" alt=""></div>
 			</div>
 		</div>
 	</div>
